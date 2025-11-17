0cb1997cab87ff3b0eed354ef81a820fb8409d88
mynuaimi <33287057+mynuaimi@users.noreply.github.com>
2025-11-17 13:27:57 +0000

    Add S3 storage plugin and config

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,15 @@
-export default () => ({});
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        endpoint: env('AWS_ENDPOINT'),
+        accessKeyId: env('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: env('AWS_ACCESS_SECRET'),
+        params: {
+          Bucket: env('AWS_BUCKET'),
+        },
+      },
+    },
+  },
+});
