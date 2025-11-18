e7dd3f48101773a170789e290719e2261f9731af
mynuaimi <33287057+mynuaimi@users.noreply.github.com>
2025-11-18 07:59:06 +0000

    Fix S3 config structure

--- config/plugins.ts
+++ config/plugins.ts
@@ -3,11 +3,14 @@ module.exports = ({ env }) => ({
     config: {
       provider: 'aws-s3',
       providerOptions: {
-        endpoint: env('AWS_ENDPOINT'),
-        accessKeyId: env('AWS_ACCESS_KEY_ID'),
-        secretAccessKey: env('AWS_ACCESS_SECRET'),
-        params: {
-          Bucket: env('AWS_BUCKET'),
+        s3Options: { // <-- This is the new wrapper
+          endpoint: env('AWS_ENDPOINT'),
+          accessKeyId: env('AWS_ACCESS_KEY_ID'),
+          secretAccessKey: env('AWS_ACCESS_SECRET'),
+          region: env('AWS_REGION'), // Add this line
+          params: {
+            Bucket: env('AWS_BUCKET'),
+          },
         },
       },
     },
