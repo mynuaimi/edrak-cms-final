d8fd5d6c217506f9144bceb3abdcc073272b50be
mynuaimi <33287057+mynuaimi@users.noreply.github.com>
2025-11-18 08:24:33 +0000

    correcting database EV

--- config/database.ts
+++ config/database.ts
@@ -1,35 +1,20 @@
-// config/database.js
 const { parse } = require('pg-connection-string');
 
 module.exports = ({ env }) => {
-  // If a DATABASE_URL is provided, parse it and use PostgreSQL
-  if (env('DATABASE_URL')) {
-    const { host, port, database, user, password } = parse(env('DATABASE_URL'));
+  const dbUrl = env('DATABASE_URL');
+  const config = parse(dbUrl);
 
-    return {
-      connection: {
-        client: 'postgres',
-        connection: {
-          host,
-          port,
-          database,
-          user,
-          password,
-          ssl: { rejectUnauthorized: false }, 
-        },
-      },
-    }
-  }
-
-  // Fallback for build-time or local development:
-  // Use a simple SQLite database
   return {
     connection: {
-      client: 'sqlite',
+      client: 'postgres',
       connection: {
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        user: config.user,
+        password: config.password,
+        ssl: true,
       },
-      useNullAsDefault: true,
     },
   };
 };
