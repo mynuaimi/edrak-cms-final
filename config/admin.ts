9922cc70fe8d0f78508f0af7128c2757290129f5
mynuaimi <33287057+mynuaimi@users.noreply.github.com>
2025-11-18 22:35:58 +0400

    Add session management configuration options

--- config/admin.ts
+++ config/admin.ts
@@ -1,6 +1,11 @@
 export default ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
+    sessions: {
+      // in seconds
+      maxRefreshTokenLifespan: env.int('ADMIN_MAX_REFRESH_TOKEN_LIFESPAN', 30 * 24 * 60 * 60), // 30 days
+      maxSessionLifespan: env.int('ADMIN_MAX_SESSION_LIFESPAN', 7 * 24 * 60 * 60), // 7 days
+    },
   },
   apiToken: {
     salt: env('API_TOKEN_SALT'),
