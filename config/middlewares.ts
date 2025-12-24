0bb3f5857287abf9bc737e8667da86c483f2621d
mynuaimi <33287057+mynuaimi@users.noreply.github.com>
2025-12-24 22:19:08 +0400

    Update middlewares.ts

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,7 +1,34 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          // This allows Strapi to load and display images from your DO Spaces
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'edrak-ajman.fra1.digitaloceanspaces.com',
+          ],
+          // This allows Strapi to handle video/audio from your DO Spaces
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'edrak-ajman.fra1.digitaloceanspaces.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
